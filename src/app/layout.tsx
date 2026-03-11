d400e83769266f401aa610e466fcaad03d92dc82
Salva Carsi Moreno <salvavcarsi@gmail.com>
2026-03-11 17:37:14 +0100

    firs commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Claude App",
+  description: "Hello World con Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
